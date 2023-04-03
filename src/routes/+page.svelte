cbd162f48b3b8b009a3fb3ee103abbe5df0136a2
Suneeth <suneeth.since2005@gmail.com>
2023-04-03 14:58:18 +0530

    Added contitional rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,10 @@
   ];
 
   const handleRemovePerson = (id: Number) => {
-    console.log(id)
+    console.log(id);
     // remote person from array
-    people = people.filter((person) => person.id !== id)
-  }
+    people = people.filter((person) => person.id !== id);
+  };
 </script>
 
 <svelte:head>
@@ -22,11 +22,23 @@
   <div class="w-screen h-screen flex items-center flex-col pt-48">
     {#each people as person (person.id)}
       <div class="py-3">
-        <h2 class="text-xl font-semibold">
-          {person.name}
-        </h2>
+        <div class="flex items-center gap-3 justify-between">
+          <h2 class="text-xl font-semibold">
+            {person.name}
+          </h2>
+          {#if person.age > 30}
+            <span class="text-sm opacity-60">Senior devloper</span>
+          {:else if person.age > 20}
+            <span class="text-sm opacity-60">Junior devloper</span>
+          {:else}
+            <span class="text-sm opacity-60">Newbie</span>
+          {/if}
+        </div>
         <h4>{person.age} year old {person.role} developer.</h4>
-        <button on:click={() => handleRemovePerson(person.id)} class="bg-stone-100 p-1 px-3 rounded mt-1">Remove</button>
+        <button
+          on:click={() => handleRemovePerson(person.id)}
+          class="bg-stone-100 p-1 px-3 rounded mt-1">Remove</button
+        >
       </div>
     {:else}
       <p>There is no people left in the array!</p>
