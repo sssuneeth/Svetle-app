6d971c6961e72130a271533997b38b892c334dc8
Suneeth <suneeth.since2005@gmail.com>
2023-04-03 14:38:16 +0530

    Implemented inline event handlers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,35 @@
-<script>
+<script lang="ts">
   import Layout from "./+layout.svelte";
 
-  let array = [
-    { title: 'Learning svelte', id:1 },
-    { title: 'Contributing opensource', id:2 },
-    { title: 'Doing freelance jobs', id:3 }
+  let people = [
+    { name: "John Doe", age: 23, role: "full stack", id: 1 },
+    { name: "Suneeth", age: 36, role: "frontend", id: 2 },
+    { name: "Foo man", age: 12, role: "backend", id: 3 },
   ];
+
+  const handleRemovePerson = (id: Number) => {
+    console.log(id)
+    // remote person from array
+    people = people.filter((person) => person.id !== id)
+  }
 </script>
+
+<svelte:head>
+  <title>Svetle-app</title>
+</svelte:head>
 
 <main>
   <div class="w-screen h-screen flex items-center flex-col pt-48">
-    {#each array as item (item.id)}
-      <h2 class="text-xl font-semibold underline underline-offset-8 decoration-stone-300 py-2">{item.id}. {item.title}</h2>
+    {#each people as person (person.id)}
+      <div class="py-3">
+        <h2 class="text-xl font-semibold">
+          {person.name}
+        </h2>
+        <h4>{person.age} year old {person.role} developer.</h4>
+        <button on:click={() => handleRemovePerson(person.id)} class="bg-stone-100 p-1 px-3 rounded mt-1">Remove</button>
+      </div>
     {:else}
-      <p>There is no items left in the array!</p>
+      <p>There is no people left in the array!</p>
     {/each}
   </div>
 </main>
