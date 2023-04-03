79a9201ce48bc31c494425cd098d27d563b6199c
Suneeth <suneeth.since2005@gmail.com>
2023-04-03 15:23:14 +0530

    Added components with contitional rendering

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,9 @@
+<script>
+  let showModal = true;
+</script>
+
+{#if showModal}
+  <main>
+    <h3 class="text-2xl font-semibold underline decoration-slate-200 underline-offset-[6px]">This is modal</h3>
+  </main>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Layout from "./+layout.svelte";
+  import Modal from "../lib/components/Modal.svelte";
 
   let people = [
     { name: "John Doe", age: 23, role: "full stack", id: 1 },
@@ -19,29 +20,32 @@
 </svelte:head>
 
 <main>
-  <div class="w-screen h-screen flex items-center flex-col pt-48">
-    {#each people as person (person.id)}
-      <div class="py-3">
-        <div class="flex items-center gap-3 justify-between">
-          <h2 class="text-xl font-semibold">
-            {person.name}
-          </h2>
-          {#if person.age > 30}
-            <span class="text-sm opacity-60">Senior devloper</span>
-          {:else if person.age > 20}
-            <span class="text-sm opacity-60">Junior devloper</span>
-          {:else}
-            <span class="text-sm opacity-60">Newbie</span>
-          {/if}
+  <div class="w-screen h-screen flex items-center flex-col pt-20">
+    <div class="border p-7 rounded-md w-[350px]">
+      <Modal />
+      {#each people as person (person.id)}
+        <div class="py-3">
+          <div class="flex items-center gap-3">
+            <h2 class="text-xl font-semibold">
+              {person.name}
+            </h2>
+            {#if person.age > 30}
+              <span class="text-sm opacity-60">Senior devloper</span>
+            {:else if person.age > 20}
+              <span class="text-sm opacity-60">Junior devloper</span>
+            {:else}
+              <span class="text-sm opacity-60">Newbie</span>
+            {/if}
+          </div>
+          <h4>{person.age} year old {person.role} developer.</h4>
+          <button
+            on:click={() => handleRemovePerson(person.id)}
+            class="bg-stone-100 p-2 px-5 rounded mt-2 text-sm">Remove</button
+          >
         </div>
-        <h4>{person.age} year old {person.role} developer.</h4>
-        <button
-          on:click={() => handleRemovePerson(person.id)}
-          class="bg-stone-100 p-1 px-3 rounded mt-1">Remove</button
-        >
-      </div>
-    {:else}
-      <p>There is no people left in the array!</p>
-    {/each}
+      {:else}
+        <p>There is no people left in the array!</p>
+      {/each}
+    </div>
   </div>
 </main>
