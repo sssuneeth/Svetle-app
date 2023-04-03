4107f92b6d2ca953a36820641cb143905d6bd4a2
Suneeth <suneeth.since2005@gmail.com>
2023-04-03 13:27:00 +0530

    Implemented looping with array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,19 @@
 <script>
   import Layout from "./+layout.svelte";
 
-  let color = "Black";
-  let firstName = "John";
-  let lastName = "Doe";
-  // reactive for getting fullName
-  $: fullName = `${firstName} ${lastName}`;
+  let array = [
+    { title: 'Learning svelte', id:1 },
+    { title: 'Contributing opensource', id:2 },
+    { title: 'Doing freelance jobs', id:3 }
+  ];
 </script>
 
 <main>
   <div class="w-screen h-screen flex items-center flex-col pt-48">
-    <h2 class="text-xl">{fullName} - {color} belt</h2>
-    <div>
-      <input
-        type="text"
-        class="border-2 pl-3 p-1 mt-2 outline-none"
-        bind:value={firstName}
-      />
-      <input
-        type="text"
-        class="border-2 pl-3 p-1 mt-2 outline-none"
-        bind:value={lastName}
-      />
-      <input
-        type="text"
-        class="border-2 pl-3 p-1 mt-2 outline-none"
-        bind:value={color}
-      />
-    </div>
+    {#each array as item (item.id)}
+      <h2 class="text-xl font-semibold underline underline-offset-8 decoration-stone-300 py-2">{item.id}. {item.title}</h2>
+    {:else}
+      <p>There is no items left in the array!</p>
+    {/each}
   </div>
 </main>
